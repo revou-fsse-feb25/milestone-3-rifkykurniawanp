28dd961a682b1b0ff397a87c62b505337f3bc71e
rifky <krifky14@yahoo.co.id>
2025-05-20 17:47:42 +0700

    adding

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="p4 bg-amber-200 w-screen h-screen flex flex-col justify-center items-center text-black">
+      <h1 style={{ fontSize: '3rem' }}>404 - Halaman Tidak Ditemukan</h1>
+      <p>Nyari apa hayo.</p>
+    </div>
+  );
+}
