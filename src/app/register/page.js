502cba5cb7944508ab15b4e5b5248b9ae96b4dd5
rifky <krifky14@yahoo.co.id>
2025-05-23 14:08:11 +0700

    register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -45,8 +45,8 @@ const Register = () => {
             <h2 className="text-2xl p-3 flex justify-center">Register</h2>
             {error && <p className="text-red-500 ">{error}</p>}
             <br />
-            
-            <form className="max-w-md mx-auto bg-pink-300 p-8 rounded-lg shadow-md text-black">
+
+            <form onSubmit={(e) => handleRegister(e)} className="max-w-md mx-auto bg-pink-300 p-8 rounded-lg shadow-md text-black">
             <div className="relative z-0 w-full mb-5 group">
                 <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                 <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
